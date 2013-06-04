917cc22423b0bd5e05ba52a8ec0b37136d9fddfd
IgorDePaula <principe.borodin@gmail.com>
2013-06-04 11:51:21 -0300

    Inclusao das acl

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Compras;
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -8,6 +8,16 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 return array(
+      'acl' => array(
+        'Compras' => array(
+            'COMPRAS' => array(
+                'Almoxarifado\Controller\Index:index',              
+            ),
+            'TI' => array(
+                'Almoxarifado\Controller\Index:index',              
+            ),
+        )
+    ),
     'router' => array(
         'routes' => array(
             // This defines the hostname route which forms the base
